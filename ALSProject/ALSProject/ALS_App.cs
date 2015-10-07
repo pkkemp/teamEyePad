0a2699dffabc1a636213b3bd7e3f88dd829f56bb
allison <allison.chilton@eagles.oc.edu>
2015-10-07 18:09:36 -0500

    Created an Official ALS App (it's empty)

---
+++ ALSProject/ALSProject/ALS_App.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ALSProject
+{
+    static class ALS_App
+    {
+        //main loop
+        static void Main(String[] args){
+            
+
+        }
+    }
+}
