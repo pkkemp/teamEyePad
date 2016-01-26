5bfd7918e85d63a0a6eb33515905077c632236b8
allison <allison.chilton@eagles.oc.edu>
2016-01-25 20:07:50 -0600

    Test email

--- ALSProject/Email.cs
+++ ALSProject/Email.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -13,10 +14,17 @@ namespace ALSProject
     public partial class Email : Form
     {
         Form parent;
+        EmailClient client;
+
 
         public Email(Form parent)
         {
             InitializeComponent();
+<<<<<<< Upstream, based on origin/master
+=======
+            client = new EmailClient();
+            this.CreateLayout(new System.ComponentModel.ComponentResourceManager(typeof(Email)));
+>>>>>>> f2ab486 Test email
             this.parent = parent;
         }
 
@@ -26,5 +34,17 @@ namespace ALSProject
             this.Hide();
 
         }
+<<<<<<< Upstream, based on origin/master
+=======
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            client.sendMessage(new MailMessage("teameyepad@gmail.com", "allison.chilton@outlook.com", "Test Email", "This is the body of the test."));
+
+        }
+
+
+
+>>>>>>> f2ab486 Test email
     }
 }
---
+++ ALSProject/EmailClient.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ALSProject
+{
+    class EmailClient
+    {
+        SmtpClient sendClient;
+
+        public EmailClient()
+        {
+            StartSMTP();
+        }
+
+        public void StartSMTP()
+        {
+            sendClient = new SmtpClient("smtp.gmail.com");
+            sendClient.Port = 587;
+            sendClient.Credentials =
+            new System.Net.NetworkCredential("teameyepad", "highEyeGuy");
+            sendClient.EnableSsl = true;
+        }
+
+        public void sendMessage(MailMessage mail)
+        {
+            sendClient.Send(mail);
+            //Console.WriteLine("Sending email");
+        }
+
+        public void retrieveMail()
+        {
+
+        }
+
+    }
+}
